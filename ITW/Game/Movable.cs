4e09e9f0a27d92f36b51621840ad47072252cfa1
SkillGG <github@skilletss.cba.pl>
2019-04-24 17:06:58 +0200

    File Movements and added SaveState to work on
    Moved some files into folder to not trash main dir
    Added SaveState to make saving the game in future

---
+++ ITW/Game/Movable.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace ITW {
+	interface IMovable {
+		void SetVelocity(float x, float y);
+		Vector2 GetVelocity();
+		void MoveTick();
+		void MoveBy(float x, float y);
+		void MoveBy(Vector2 v);
+		Point GetPosition();
+	}
+}
---
+++ ITW/Game/SaveState.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Xna.Framework.Design;
+
+namespace ITW{
+
+	public class SaveState {
+
+		private bool Load { get; set; }
+
+		public SaveState(){
+			//init savestate
+			Load = false;
+		}
+
+		public void LoadFromFile(string path){
+
+			
+
+		}
+
+	}
+
+}
