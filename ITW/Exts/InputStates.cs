a54ebd3c1fee6978d8b37a25f0086b5407f6e59c
SkillGG <github@skilletss.cba.pl>
2019-07-05 13:01:24 +0200

    Commented InputStates

--- ITW/Exts/InputStates.cs
+++ ITW/Exts/InputStates.cs
@@ -3,6 +3,9 @@ using Microsoft.Xna.Framework.Input;
 
 namespace ITW.Exts {
 
+	/// <summary>
+	/// Possible mouse clicks
+	/// </summary>
 	public enum MouseButton {
 		RIGHT,
 		LEFT,
@@ -10,10 +13,25 @@ namespace ITW.Exts {
 		NONE
 	}
 
+	/// <summary>
+	/// A class that holds information about an instance of mouse click
+	/// </summary>
 	public class MouseClick {
 
+		/// <summary>
+		/// <see cref="MouseButton"/> which was pressed on mouse
+		/// </summary>
 		public MouseButton Button { get; private set; }
+
+		/// <summary>
+		/// Position of the mouse
+		/// </summary>
 		public Point Point { get; private set; }
+
+		/// <summary>
+		/// Rectangle at mouse's position to use <see cref="Rectangle.Intersects(Rectangle)"/>
+		/// </summary>
+		public Rectangle Rectangle { get => new Rectangle(Point, new Point(1, 1)); private set { } }
 
 		private MouseClick() { }
 
@@ -29,19 +47,41 @@ namespace ITW.Exts {
 
 	}
 
+	/// <summary>
+	/// Class that takes care of every kind of input.
+	/// </summary>
 	public class InputStates {
 
+		/// <summary>
+		/// MonoGame's <see cref="MouseState"/>.
+		/// </summary>
 		public MouseState mouse;
+		/// <summary>
+		/// MonoGame's <see cref="KeyboardState"/>.
+		/// </summary>
 		public KeyboardState keyboard;
+		/// <summary>
+		/// MonoGame's <see cref="GamePadState"/>. (Player 1)
+		/// </summary>
 		public GamePadState gamePad1;
+		/// <summary>
+		/// MonoGame's <see cref="GamePadState"/>. (Player 2)
+		/// </summary>
 		public GamePadState gamePad2;
-
+		
+		/// <summary>
+		/// Mouse position as a rectangle.
+		/// </summary>
 		public Rectangle MouseRectangle {
 			get {
 				return new Rectangle(mouse.Position, new Point(1, 1));
 			}
 		}
 
+		/// <summary>
+		/// First initialization of InputState
+		/// <para>Best used in: <see cref="ITW.Initialize()"/></para>
+		/// </summary>
 		public InputStates() {
 			this.mouse = Mouse.GetState( );
 			this.keyboard = Keyboard.GetState( );
@@ -49,6 +89,10 @@ namespace ITW.Exts {
 			this.gamePad2 = GamePad.GetState(PlayerIndex.Two);
 		}
 
+		/// <summary>
+		/// Refreshing all mouse states
+		/// <para>Best used in: <see cref="ITW.Update(GameTime)"/> (AFTER <code>base.Update()</code>)</para>
+		/// </summary>
 		public void Update() {
 			this.mouse = Mouse.GetState( );
 			this.keyboard = Keyboard.GetState( );
@@ -59,18 +103,33 @@ namespace ITW.Exts {
 
 		// KEYS
 
+		/// <summary>
+		/// Check if given key is pressed (state check)
+		/// </summary>
+		/// <param name="k"></param>
+		/// <returns></returns>
 		public bool IsKeyDown(Keys k) { return this.keyboard.IsKeyDown(k); }
+		/// <summary>
+		/// Check if given key is released (state check)
+		/// </summary>
 		public bool IsKeyUp(Keys k) { return this.keyboard.IsKeyUp(k); }
 
 		// KeyChange
+
+		/// <summary>
+		/// Check if given key's state has changed (fires every change).
+		/// </summary>
 		public bool KeyChange(InputStates b, Keys k) {
 			this.Update( );
-			if( b.keyboard.IsKeyDown(k) != this.keyboard.IsKeyDown(k) )
+			if( b.keyboard.IsKeyDown(k) != IsKeyDown(k) )
 				return true;
 			return false;
 		}
 
 		// KeyUp
+		/// <summary>
+		/// Check if given key's state has been released (fires once).
+		/// </summary>
 		public bool KeyUp(InputStates b, Keys k) {
 			if( KeyChange(b, k) )
 				if( IsKeyUp(k) )
@@ -79,6 +138,9 @@ namespace ITW.Exts {
 		}
 
 		// KeyDown
+		/// <summary>
+		/// Check if given key's state has been pressed (fires once).
+		/// </summary>
 		public bool KeyDown(InputStates b, Keys k) {
 			if( KeyChange(b, k) )
 				if( IsKeyDown(k) )
@@ -90,6 +152,12 @@ namespace ITW.Exts {
 		// BUTTONS
 
 		// IsButtonDown
+		/// <summary>
+		/// Check if pad button is pressed (state check)
+		/// <para>Returns player's pad ID or 3 (if both players).</para>
+		/// </summary>
+		/// <param name="k">Button to check for</param>
+		/// <returns>player's pad ID or 3 (if both players)</returns>
 		public byte IsButtonDown(Buttons k) {
 			this.Update( );
 			if( gamePad1.IsButtonDown(k) ) {
@@ -100,11 +168,23 @@ namespace ITW.Exts {
 				return 2;
 			return 0;
 		}
+
+		/// <summary>
+		/// Checks if button is pressed on any gamepad
+		/// </summary>
+		/// <param name = "k" >Button to check for</param>
+		/// <returns></returns>
 		public bool IsButtonDownAny(Buttons k) {
 			return ( IsButtonDown(k) != 0 );
 		}
 
 		// IsButtonUp
+		/// <summary>
+		/// Check if pad button is released (state check)
+		/// <para>Returns player's pad ID or 3 (if both players).</para>
+		/// </summary>
+		/// <param name="k">Button to check for</param>
+		/// <returns>player's pad ID or 3 (if both players)</returns>
 		public byte IsButtonUp(Buttons k) {
 			this.Update( );
 			if( gamePad1.IsButtonUp(k) ) {
@@ -115,11 +195,23 @@ namespace ITW.Exts {
 				return 2;
 			return 0;
 		}
+
+		/// <summary>
+		/// Checks if button is released on any gamepad
+		/// </summary>
+		/// <param name = "k" >Button to check for</param>
+		/// <returns></returns>
 		public bool IsButtonUpAny(Buttons k) {
 			return ( IsButtonUp(k) != 0 );
 		}
 
 		// Button Change
+		/// <summary>
+		/// Check if pad button's state changed (fires every change)
+		/// <para>Returns player's pad ID or 3 (if both players).</para>
+		/// </summary>
+		/// <param name="k">Button to check for</param>
+		/// <returns>player's pad ID or 3 (if both players)</returns>
 		public byte ButtonChange(InputStates b, Buttons k) {
 			this.Update( );
 			if( b.gamePad1.IsButtonDown(k) != this.gamePad1.IsButtonDown(k) ) {
@@ -131,17 +223,35 @@ namespace ITW.Exts {
 				return 2;
 			return 0;
 		}
+
+		/// <summary>
+		/// Checks if button's state changed on any gamepad (fires every change)
+		/// </summary>
+		/// <param name = "k" >Button to check for</param>
+		/// <returns></returns>
 		public bool ButtonChangeAny(InputStates b, Buttons k) {
 			return ( ButtonChange(b, k) != 0 );
 		}
 
 		// ButtonDown
+		/// <summary>
+		/// Check if pad button has been pressed (fires once)
+		/// <para>Returns player's pad ID or 3 (if both players).</para>
+		/// </summary>
+		/// <param name="k">Button to check for</param>
+		/// <returns>player's pad ID or 3 (if both players)</returns>
 		public byte ButtonDown(InputStates b, Buttons k) {
 			byte bc = ButtonChange(b, k);
 			if( bc != 0 && bc == IsButtonDown(k) )
 				return bc;
 			return 0;
 		}
+
+		/// <summary>
+		/// Checks if button has been pressed on any gamepad (fires once)
+		/// </summary>
+		/// <param name = "k" >Button to check for</param>
+		/// <returns></returns>
 		public bool ButtonDownAny(InputStates b, Buttons k) {
 			if( ButtonChangeAny(b, k) )
 				if( IsButtonDownAny(k) )
@@ -150,12 +260,24 @@ namespace ITW.Exts {
 		}
 
 		// ButtonUp
+		/// <summary>
+		/// Check if pad button has been released (fires once)
+		/// <para>Returns player's pad ID or 3 (if both players).</para>
+		/// </summary>
+		/// <param name="k">Button to check for</param>
+		/// <returns>player's pad ID or 3 (if both players)</returns>
 		public byte ButtonUp(InputStates b, Buttons k) {
 			byte bc = ButtonChange(b, k);
 			if( bc != 0 && bc == IsButtonUp(k) )
 				return bc;
 			return 0;
 		}
+
+		/// <summary>
+		/// Checks if button has been released on any gamepad (fires once)
+		/// </summary>
+		/// <param name = "k" >Button to check for</param>
+		/// <returns></returns>
 		public bool ButtonUpAny(InputStates b, Buttons k) {
 			if( ButtonChangeAny(b, k) )
 				if( IsButtonUpAny(k) )
